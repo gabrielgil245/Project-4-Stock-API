8a3c6f3141fcd38ad1a5b204226c3d22a3db115c
gabrielgil245 <gabrielgil245@gmail.com>
2021-03-14 00:27:11 -0600

    Get routers set up for users to read the cash and portfolio items.

--- routes/cash.js
+++ routes/cash.js
@@ -3,21 +3,10 @@ var router = express.Router();
 const {Cash} = require('../lib/models');
 
 /* GET cash balance. */
-router.get('/', function(req, res, next) {
-  res.send('respond with cash');
-});
-
-// UPDATE - perform UPDATE request on http://localhost:3000/api/v1/cash/:id
-router.put('/:id', function(req, res, next) {
-  console.log(req.body)
-  console.log(req.params)
-
-  // let cash = await Cash.update(req.body, {
-  //   where: req.params.id
-  // });
-  // res.json({cash});
-
-  res.json({success: true});
+router.get('/', async function(req, res, next) {
+  let currentCash = await Cash.findOne({})
+  console.log(currentCash);
+  res.json(currentCash);
 });
 
 module.exports = router;
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -6,12 +6,30 @@ const yahooStockPrices = require('yahoo-stock-prices');
 // CREATE - perform POST request on http://localhost:3000/api/v1/portfolio
 router.post('/', async function(req, res, next) {
   console.log(req.body);
-
   let stock = await Portfolio.create(req.body);
   res.json({stock});
 });
 
-// READ - perform GET request on http://localhost:3000/api/v1/portfolio/search/:id
+
+// READ - perform GET request on http://localhost:3000/api/v1/portfolio
+router.get('/', async function(req, res, next) {
+  let items = await Portfolio.findAll({})
+  console.log(items);
+  res.json(items);
+});
+
+// READ - perform GET request on http://localhost:3000/api/v1/portfolio/:symbol
+router.get('/:symbol', async function(req, res, next) {
+  let stock = await Portfolio.findAll({
+    where: {
+      symbol: req.params.symbol
+    }
+  })
+  console.log(stock);
+  res.json(stock);
+});
+
+// READ - perform GET request on http://localhost:3000/api/v1/portfolio/search/:symbol
 router.get('/search/:symbol', async function(req, res, next) {
   console.log(req.query);
   console.log(req.params);
@@ -42,12 +60,5 @@ router.delete('/:id', async function(req, res, next) {
   let stock = await Portfolio.destroy({where: {id: req.params.id}});
   res.json({stock});
 });
-
-// READ - perform GET request on http://localhost:3000/api/v1/portfolio
-// router.get('/', async function(req, res, next) {
-//   let items = await Portfolio.findAll({})
-//   console.log(items);
-//   res.json(items);
-// });
 
 module.exports = router;
