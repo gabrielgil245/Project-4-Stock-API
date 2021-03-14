944d561d1a3ad98f4168d0994fa66d80e5d02078
gabrielgil245 <gabrielgil245@gmail.com>
2021-03-14 17:43:02 -0500

    PUT request created in cash.js for updating cash tally.

--- routes/cash.js
+++ routes/cash.js
@@ -9,4 +9,15 @@ router.get('/', async function(req, res, next) {
   res.json(currentCash);
 });
 
+// UPDATE - perform PUT request on http://localhost:3000/api/v1/cash/:id
+router.put('/:id', async function(req, res, next) {
+  console.log(req.body)
+  console.log(req.params)
+
+  let stock = await Cash.update(req.body, {
+    where: {id: req.params.id}
+  });
+  res.json({stock})
+});
+
 module.exports = router;
